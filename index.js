219cd1b6b25c062ef87e116b07591f5b59a43094
Shariq <ansari.shariq.badshah@gmail.com>
2023-12-09 23:59:53 -0800

    mongodb connected

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+import {registerUser} from './controllers/user.controler.js'
 
 const app = express();
 const port = 3000; 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.post('/createuser', (req, res) => {
-    res.json({
-        "status":200,
-        "message":"User Added Successfully"
-    })
-   
-  });
+mongoose.connect('mongodb+srv://Shariq:Tetrahedral135@cluster0.og22izb.mongodb.net/')
+  .then(() => console.log('Connected!'))
+  .catch((error)=>{
+    console.log("eeroor",error);
+  })
+
+app.post('/createuser',registerUser);
   
 
 app.listen(port, () => {
